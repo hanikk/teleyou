97f88e4cf621b1a8f8f8e5b0be67d2aa6cd54118
hanikk <hanikarim77@gmail.com>
2016-02-14 23:52:05 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {142627841},--Sudo users
+    sudo_users = {149516204},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
